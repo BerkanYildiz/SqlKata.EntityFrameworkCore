f6c932db92ab7b9c2cb078f9038db9661e8a6e78
BerkanYildiz <BerkanYildiz@users.noreply.github.com>
2022-05-10 01:31:45 +0200

    Asynchronous ExecuteSqlRaw, and Query.From based on model type

--- src/SqlKata.EntityFrameworkCore/DatabaseFacadeExtensions.cs
+++ src/SqlKata.EntityFrameworkCore/DatabaseFacadeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace SqlKata.EntityFrameworkCore
 {
     using System;
+    using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -61,6 +62,60 @@
         {
             SqlKataEntityFramework.LastUsedCompiler = InCompiler;
             return ExecuteSqlKata(This, InCompiler.Compile(InQuery(new Query())));
+        }
+
+        /// <summary>
+        /// Executes a raw SQL query against the database.
+        /// </summary>
+        /// <param name="This">The database facade.</param>
+        /// <param name="InCompiledQuery">The compiled query.</param>
+        public static Task<int> ExecuteSqlKataAsync(this DatabaseFacade This, SqlResult InCompiledQuery)
+        {
+            return This.ExecuteSqlRawAsync(InCompiledQuery.Sql, InCompiledQuery.Bindings.ToArray());
+        }
+
+        /// <summary>
+        /// Executes a raw SQL query against the database.
+        /// </summary>
+        /// <param name="This">The database facade.</param>
+        /// <param name="InQuery">The query.</param>
+        public static Task<int> ExecuteSqlKataAsync(this DatabaseFacade This, Query InQuery)
+        {
+            return ExecuteSqlKataAsync(This, SqlKataEntityFramework.DefaultCompiler, InQuery);
+        }
+
+        /// <summary>
+        /// Executes a raw SQL query against the database.
+        /// </summary>
+        /// <param name="This">The database facade.</param>
+        /// <param name="InCompiler">The query compiler.</param>
+        /// <param name="InQuery">The query.</param>
+        public static Task<int> ExecuteSqlKataAsync(this DatabaseFacade This, Compiler InCompiler, Query InQuery)
+        {
+            SqlKataEntityFramework.LastUsedCompiler = InCompiler;
+            return ExecuteSqlKataAsync(This, InCompiler.Compile(InQuery));
+        }
+
+        /// <summary>
+        /// Executes a raw SQL query against the database.
+        /// </summary>
+        /// <param name="This">The database facade.</param>
+        /// <param name="InQuery">The query.</param>
+        public static Task<int> ExecuteSqlKataAsync(this DatabaseFacade This, Func<Query, Query> InQuery)
+        {
+            return ExecuteSqlKataAsync(This, SqlKataEntityFramework.DefaultCompiler, InQuery(new Query()));
+        }
+
+        /// <summary>
+        /// Executes a raw SQL query against the database.
+        /// </summary>
+        /// <param name="This">The database facade.</param>
+        /// <param name="InCompiler">The query compiler.</param>
+        /// <param name="InQuery">The query.</param>
+        public static Task<int> ExecuteSqlKataAsync(this DatabaseFacade This, Compiler InCompiler, Func<Query, Query> InQuery)
+        {
+            SqlKataEntityFramework.LastUsedCompiler = InCompiler;
+            return ExecuteSqlKataAsync(This, InCompiler.Compile(InQuery(new Query())));
         }
     }
 }
---
+++ src/SqlKata.EntityFrameworkCore/QueryExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace SqlKata.EntityFrameworkCore
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata;
+
+    public static class QueryExtensions
+    {
+        /// <summary>
+        /// Binds a table to the query.
+        /// </summary>
+        /// <param name="This">The query.</param>
+        /// <param name="InEntityType">The entity model.</param>
+        public static Query From(this Query This, IEntityType InEntityType)
+        {
+            return This.From(InEntityType.GetTableName());
+        }
+
+        /// <summary>
+        /// Binds a table to the query.
+        /// </summary>
+        /// <typeparam name="T">The database set's entity model.</typeparam>
+        /// <param name="This">The query.</param>
+        /// <param name="InDbSet">The database set.</param>
+        public static Query From<T>(this Query This, DbSet<T> InDbSet) where T : class
+        {
+            return This.From(InDbSet.EntityType);
+        }
+    }
+}
